6c59e950b6e876423ff018e34b478459f148a58b
William Pei <william.h.pei@gmail.com>
2021-04-13 15:43:32 +1000

    feat(icon): exports the icon component

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ export * from './components/theme/theme';
 export * from './components/theme/theme.provider';
 
 export * from './components/text/text';
+export * from './components/icon/icon';
 export * from './components/button/button';
 export * from './components/tooltip/tooltip';
