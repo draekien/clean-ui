2392dca5cbb0c867f0632d1ee3521d8fe386fc56
William Pei <william.h.pei@gmail.com>
2021-04-26 15:20:26 +1000

    feat(input): export input component in index

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@ export * from './components/theme/theme.provider';
 
 export * from './components/text/text';
 export * from './components/icon/icon';
+export * from './components/input/input';
 export * from './components/button/button';
 export * from './components/tooltip/tooltip';
