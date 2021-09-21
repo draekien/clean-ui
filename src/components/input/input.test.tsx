9a0bd21feb62978a71c3ea6419bb75412e6a2db7
William Pei <william.h.pei@gmail.com>
2021-09-21 19:22:50 +1000

    test(input): adds tests for input variant styling

--- src/components/input/input.test.tsx
+++ src/components/input/input.test.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { themedRender } from '../../helpers/testHelpers';
+import { StatusVariant } from '../../types/variants';
+import { colors } from '../theme/theme.colors';
 import { Input } from './input';
 
 describe('Input Component', () => {
@@ -7,5 +9,17 @@ describe('Input Component', () => {
     const { container } = themedRender(<Input inputId="test" label="test" />);
 
     expect(container).toMatchSnapshot();
+  });
+
+  const variants: StatusVariant[] = ['info', 'error', 'warning', 'success'];
+  variants.forEach((variant) => {
+    it(`Should render with correct styles when variant is ${variant}`, () => {
+      const { container } = themedRender(
+        <Input inputId="test" label="test" variant={variant} />
+      );
+
+      const styles = window.getComputedStyle(container.querySelector('input') as Element);
+      expect(styles.borderColor).toBe(colors[`${variant}-300`]);
+    });
   });
 });
