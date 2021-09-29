5d29629f8e354e22322bb1fe90eda99e5054d30f
William Pei <william.h.pei@gmail.com>
2021-09-29 15:17:21 +1000

    feat(icon): added tooltip prop to the icon component
    This adds native support for displaying tooltips on the icon component without having to wrap it
    inside a tooltip yourself

--- src/components/icon/icon.test.tsx
+++ src/components/icon/icon.test.tsx
@@ -1,11 +1,47 @@
 import React from 'react';
 import { themedRender } from '../../helpers/testHelpers';
-import { Icon } from './icon';
+import { Icon, IconVariant } from './icon';
 
 describe('Icon Component', () => {
-  it('Should match snapshot', () => {
-    const { container } = themedRender(<Icon name="accessibility" />);
+  const iconVariants: IconVariant[] = [
+    'filled',
+    'outlined',
+    'round',
+    'sharp',
+    'two-tone',
+  ];
 
-    expect(container).toMatchSnapshot();
+  describe('Icon variants', () => {
+    iconVariants.forEach((variant) => {
+      it(`${variant} should match snapshot`, () => {
+        const { container } = themedRender(
+          <Icon variant={variant} name="accessibility" />
+        );
+
+        expect(container).toMatchSnapshot();
+      });
+
+      it(`${variant} should have correct classname`, () => {
+        const { container } = themedRender(
+          <Icon variant={variant} name="accessibility" />
+        );
+
+        const icon = container.querySelector('i') as Element;
+        if (variant === 'filled') {
+          expect(icon.className).toContain('material-icons');
+        } else {
+          expect(icon.className).toContain(`material-icons-${variant}`);
+        }
+      });
+    });
+  });
+
+  describe('Icon with tooltip', () => {
+    it('Should render with tooltip component', () => {
+      const { container } = themedRender(<Icon name="accessibility" tooltip="tooltip" />);
+
+      expect(container).toMatchSnapshot();
+      expect(container.innerHTML).toContain('Tooltip');
+    });
   });
 });
