d90bdd9575410f7c675813d1724363995f556f2a
William Pei <william.h.pei@gmail.com>
2021-10-01 09:00:13 +1000

    feat(input): implemented input masks using react-input-mask

--- src/stories/input.stories.tsx
+++ src/stories/input.stories.tsx
@@ -15,11 +15,39 @@ Default.args = {
   placeholder: "I'm a basic input",
 };
 
+export const Disabled = Template.bind({});
+Disabled.args = {
+  inputId: 'disabled',
+  placeholder: "I'm disabled",
+  disabled: true,
+};
+
 export const WithIcon = Template.bind({});
 WithIcon.args = {
   inputId: 'withIcon',
   icon: 'mail',
   placeholder: 'I now have an icon',
+};
+
+export const WithMaskAndIcon = Template.bind({});
+WithMaskAndIcon.args = {
+  inputId: 'withMask',
+  mask: '99/99/9999',
+  icon: 'event',
+};
+
+export const ReadOnly = Template.bind({});
+ReadOnly.args = {
+  inputId: 'readonly',
+  value: 'Prefilled value',
+  readOnly: true,
+};
+
+export const Types = Template.bind({});
+Types.args = {
+  inputId: 'withType',
+  icon: 'event',
+  type: 'date',
 };
 
 export const Variants = Template.bind({});
