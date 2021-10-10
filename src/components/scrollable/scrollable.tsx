a2c28c60cca12bb7bf1a680816e92ce35e430f88
William Pei <william.h.pei@gmail.com>
2021-10-11 10:10:12 +1100

    docs: added jsdocs for some components

--- src/components/scrollable/scrollable.tsx
+++ src/components/scrollable/scrollable.tsx
@@ -2,10 +2,19 @@
 import React from 'react';
 import { Scrollbars } from 'react-custom-scrollbars-2';
 
+/** The props for the Scrollabel component
+ *  @see Scrollable
+ */
 export interface ScrollableProps {
   maxHeight?: string | number;
 }
 
+/**
+ * A pre-styled wrapper for react-custom-scrollbars-2
+ * @param props the props required by the Scrollable component
+ * @see Scrollbars from react-custom-scrollbars-2
+ * @returns the scrollable component
+ */
 export const Scrollable: React.FC<ScrollableProps> = ({ maxHeight, children }) => {
   return (
     <Scrollbars style={{ width: '100%' }} autoHeight autoHeightMax={maxHeight}>
