2f13116605b98a8c62df7b4b73607a2ebb373084
William Pei <william.h.pei@gmail.com>
2021-10-11 10:10:12 +1100

    docs: added jsdocs for some components

--- src/components/toggle/toggle.tsx
+++ src/components/toggle/toggle.tsx
@@ -3,14 +3,25 @@ import React, { forwardRef, useState, useEffect } from 'react';
 import { Label } from '../label/label';
 import { toggleContainerCss, toggleCss } from './toggle.styles';
 
+/**
+ * The props for the Toggle component
+ * @see Toggle
+ */
 export interface ToggleProps
   extends Omit<React.InputHTMLAttributes<HTMLInputElement>, 'onClick'> {
+  /** the ID of the toggle input. It will also be used as the input's name */
   inputId: string;
+  /** the component to render in the 'on' position */
   on?: React.ReactNode;
+  /** the component to render in the 'off' position */
   off?: React.ReactNode;
+  /** an event handler to be called when the toggle is checked or unchecked */
   onClick?: (checked: boolean) => void;
 }
 
+/**
+ * Renders a toggle component with the option to pass in a ref for the enclused input element.
+ */
 export const Toggle: React.ForwardRefExoticComponent<
   ToggleProps & React.RefAttributes<HTMLInputElement>
 > = forwardRef(({ on, off, inputId, disabled, onClick, ...rest }, ref) => {
