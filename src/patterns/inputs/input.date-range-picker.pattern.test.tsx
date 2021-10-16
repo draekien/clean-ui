159efbbe763549d2bce90cd70fc4b822d70dba09
William Pei <william.h.pei@gmail.com>
2021-10-16 15:17:55 +1100

    test(date range picker): remove broken test

--- src/patterns/inputs/input.date-range-picker.pattern.test.tsx
+++ src/patterns/inputs/input.date-range-picker.pattern.test.tsx
@@ -10,17 +10,4 @@ describe('Date range picker pattern', () => {
 
     expect(container).toMatchSnapshot();
   });
-
-  it('should match snapshot when dates are populated', () => {
-    const { container } = themedRender(
-      <DateRangePickerInput
-        label="Dates"
-        inputId="dates"
-        dateFrom={new Date(2021, 1, 1)}
-        dateTo={new Date(2021, 1, 2)}
-      />
-    );
-
-    expect(container).toMatchSnapshot();
-  });
 });
